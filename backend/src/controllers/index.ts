0901d836df6ffe55836ae80f51249cfd4a812d89
MagnusIskander <73327235+MagnusIskander@users.noreply.github.com>
2022-10-28 08:01:19 -0500

    build(controllers): add controllers for User and Role entities [lp4]

--- backend/src/controllers/index.ts
+++ backend/src/controllers/index.ts
@@ -1,3 +1,5 @@
 export * from './ping.controller';
 export * from './role-user.controller';
 export * from './user-role.controller';
+export * from './user.controller';
+export * from './role.controller';
