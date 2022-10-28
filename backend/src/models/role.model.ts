a83aa0894f0f72aac4d83d23c9092c691cabcc75
MagnusIskander <73327235+MagnusIskander@users.noreply.github.com>
2022-10-28 07:34:56 -0500

    build(models): add User and Role entities [lp4]

---
+++ backend/src/models/role.model.ts
@@ -0,0 +1,34 @@
+import {Entity, model, property} from '@loopback/repository';
+
+@model()
+export class Role extends Entity {
+  @property({
+    type: 'string',
+    id: true,
+    generated: true,
+  })
+  id?: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  role: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  idUsuario: string;
+
+
+  constructor(data?: Partial<Role>) {
+    super(data);
+  }
+}
+
+export interface RoleRelations {
+  // describe navigational properties here
+}
+
+export type RoleWithRelations = Role & RoleRelations;
---
+++ backend/src/models/user.model.ts
@@ -0,0 +1,92 @@
+import {Entity, model, property} from '@loopback/repository';
+
+@model()
+export class User extends Entity {
+  @property({
+    type: 'string',
+    id: true,
+    generated: true,
+  })
+  id?: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  firstName: string;
+
+  @property({
+    type: 'string',
+  })
+  secondName?: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  firstSurname: string;
+
+  @property({
+    type: 'string',
+  })
+  secondSurname?: string;
+
+  @property({
+    type: 'number',
+    required: true,
+  })
+  documentType: number;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  documentNumber: string;
+
+  @property({
+    type: 'boolean',
+    required: true,
+  })
+  sex: boolean;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  email: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  phone: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  username: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  password: string;
+
+  @property({
+    type: 'boolean',
+    required: true,
+  })
+  state: boolean;
+
+
+  constructor(data?: Partial<User>) {
+    super(data);
+  }
+}
+
+export interface UserRelations {
+  // describe navigational properties here
+}
+
+export type UserWithRelations = User & UserRelations;
